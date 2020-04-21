d3a23e9d3d9135e3a186e0d3fd295ac358316403
leina <675549518@qq.com>
2020-04-21 17:06:42 +0800

    '首页组件和request.icon'

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,13 +1,98 @@
 <template>
-  <h1>首页创建了一个Dev分支</h1>
+  <view>
+    <view class="home_tab">
+      <view class="home_tab_title">
+        <view class="title-inner">
+          <uni-segmented-control
+            :current="current"
+            :values="items.map(v=>v.title)"
+            @clickItem="onClickItem"
+            style-type="text"
+            active-color="#d4237a"
+          ></uni-segmented-control>
+        </view>
+        <view class="iconfont iconsearch"></view>
+      </view>
+      <view class="home_tab_content">
+        <view class="content">
+          <view v-if="current === 0">
+            <home-recommend></home-recommend>
+          </view>
+          <view v-if="current === 1">
+            <home-category></home-category>
+          </view>
+          <view v-if="current === 2">
+            <home-new></home-new>
+          </view>
+          <view v-if="current === 3">
+            <home-album></home-album>
+          </view>
+        </view>
+      </view>
+    </view>
+  </view>
 </template>
 
 <script>
+import homeAlbum from "./home-album";
+import homeCategory from "./home-category";
+import homeNew from "./home-new";
+import homeRecommend from "./home-recommend";
+import { uniSegmentedControl } from "@dcloudio/uni-ui";
 export default {
-
-}
+  components: {
+    homeAlbum,
+    homeCategory,
+    homeNew,
+    homeRecommend,
+    uniSegmentedControl
+  },
+  onLoad() {
+    this.request({
+      url: 'http://157.122.54.189:9088/image/v3/homepage/vertical', //开发者服务器接口地址",
+    })
+    .then(res=>{
+      console.log(res);
+      
+    })
+  },
+  data() {
+    return {
+      items: [
+        { title: "推荐" },
+        { title: "分类" },
+        { title: "最新" },
+        { title: "专辑" }
+      ],
+      current: 0
+    };
+  },
+  methods: {
+    onClickItem(e) {
+      if (this.current !== e.currentIndex) {
+        this.current = e.currentIndex;
+      }
+    }
+  }
+};
 </script>
 
-<style>
+<style lang="scss">
+  .home_tab{
+    .home_tab_title{
+      position: relative;
+      .title-inner{
+        width: 60%;
+        margin: 0 auto;
+      }
+      .iconsearch{
+        position: absolute;
+        right: 5%;
+        top: 50%;
+        transform: translateY(-50%);
 
+      }
+    }
+    .home_tab_content{}
+  }
 </style>
