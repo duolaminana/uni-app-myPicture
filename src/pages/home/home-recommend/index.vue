b2b5c481310f0fa303d1c68b49300b54c6f6fb91
leina <675549518@qq.com>
2020-04-22 13:39:17 +0800

    '推荐页面完成'

--- src/pages/home/home-recommend/index.vue
+++ src/pages/home/home-recommend/index.vue
@@ -1,5 +1,10 @@
 <template>
-  <view>
+  <scroll-view
+    scroll-y
+    class="recommend_view"
+    @scrolltolower="handleToLower"
+    v-if="recommendList.length>0"
+  >
     <!-- 推荐 -->
     <view class="recommend_wrap">
       <view class="recommend_item" v-for="item in recommendList" :key="item.id">
@@ -12,17 +17,36 @@
       <view class="monthes_title">
         <view class="monthes_title_info">
           <view class="monthes_info">
-            <text>18</text>01月
+            <text>{{monthes.DD}}/</text>
+            {{monthes.MM}}月
           </view>
+          <view class="monthes_text">{{monthes.title}}</view>
         </view>
-        <view class="monthes_text">你负责美丽</view>
+        <view class="monthes_title_more">更多></view>
       </view>
-      <view class="monthes_title_more">更多</view>
+      <view class="monthes_content">
+        <view class="monthes_item" v-for="item in monthes.items" :key="item.id">
+          <image :src="item.thumb+item.rule.replace('$<Height>',360)" mode="scaleToFill" />
+        </view>
+      </view>
     </view>
-  </view>
+
+    <!-- 热门 -->
+    <view class="hots_wrap">
+      <view class="hots_title">
+        <text>热门</text>
+      </view>
+      <view class="hots_content">
+        <view class="hot_item" v-for="item in hots" :key="item.id">
+          <image :src="item.thumb" mode="scaleToFill" />
+        </view>
+      </view>
+    </view>
+  </scroll-view>
 </template>
 
 <script>
+import moment from "moment";
 export default {
   data() {
     return {
@@ -31,23 +55,60 @@ export default {
         skip: 0,
         order: "hot"
       },
-      // 推荐列表
-      recommendList: []
+      recommendList: [], // 推荐列表
+      monthes: {}, //月份
+      hots: [], //热门
+      hasMore: true
     };
   },
   mounted() {
-    this.request({
-      url: "http://157.122.54.189:9088/image/v3/homepage/vertical",
-      data: this.params
-    }).then(res => {
-      console.log(res);
-      this.recommendList = res.res.homepage[1].items;
-    });
+    this.getData();
+  },
+  methods: {
+    handleToLower() {
+      if (this.hasMore) {
+        this.params.skip += this.params.limit;
+        this.getData();
+      } else {
+        uni.showToast({
+          title: "没有更多数据了",
+          icon: "none"
+        });
+      }
+    },
+    getData() {
+      this.request({
+        url: "http://157.122.54.189:9088/image/v3/homepage/vertical",
+        data: this.params
+      }).then(result => {
+        console.log(result);
+        // 判断有没有下一页数据
+        if (result.res.vertical.length == 0) {
+          this.hasMore = false;
+          uni.showToast({
+            title: "没有更多数据了",
+            icon: "none"
+          });
+          return;
+        }
+
+        if (this.hots.length == 0) {
+          this.recommendList = result.res.homepage[1].items;
+          this.monthes = result.res.homepage[2];
+          this.monthes.MM = moment(this.monthes.stime).format("MM");
+          this.monthes.DD = moment(this.monthes.stime).format("DD");
+        }
+        this.hots = [...this.hots, ...result.res.vertical];
+      });
+    }
   }
 };
 </script>
 
 <style lang="scss">
+.recommend_view {
+  height: calc(100vh - 36px);
+}
 .recommend_wrap {
   display: flex;
   flex-wrap: wrap;
@@ -59,21 +120,64 @@ export default {
 
 .monthes_wrap {
   .monthes_title {
+    display: flex;
+    justify-content: space-between;
+    padding: 20rpx;
+
     .monthes_title_info {
+      color: #d83a88;
+      font-size: 30rpx;
+      font-weight: 600;
+      display: flex;
       .monthes_info {
         text {
-
+          font-size: 36rpx;
         }
       }
     }
 
     .monthes_text {
-
+      font-size: 34rpx;
+      color: #666;
+      margin-left: 30rpx;
+    }
+    .monthes_title_more {
+      font-size: 24rpx;
+      color: #d83a88;
     }
   }
 
-  .monthes_title_more {
+  .monthes_content {
+    display: flex;
+    flex-wrap: wrap;
+    .monthes_item {
+      width: 33.33%;
+      border: 5rpx solid #fff;
+    }
+  }
+}
 
+.hots_wrap {
+  .hots_title {
+    padding: 20rpx;
+    text {
+      border-left: 20rpx solid #d83a88;
+      padding-left: 20rpx;
+      font-size: 34rpx;
+      font-weight: 600;
+    }
+  }
+
+  .hots_content {
+    display: flex;
+    flex-wrap: wrap;
+
+    .hot_item {
+      width: 33.33%;
+      border: 5rpx solid #fff;
+      image {
+      }
+    }
   }
 }
 </style>
